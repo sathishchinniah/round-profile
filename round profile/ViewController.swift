5512ce5a65e32201b2ac2a868572ec49447bbd0f
sathish chinniah <sathish73ec@gmail.com>
2015-10-08 00:23:22 +0530

    Initial Commit

---
+++ round profile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  round profile
+//
+//  Created by Sathish Chinniah on 08/10/15.
+//  Copyright © 2015 Sathish Chinniah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ round profileTests/round_profileTests.swift
@@ -0,0 +1,36 @@
+//
+//  round_profileTests.swift
+//  round profileTests
+//
+//  Created by Sathish Chinniah on 08/10/15.
+//  Copyright © 2015 Sathish Chinniah. All rights reserved.
+//
+
+import XCTest
+@testable import round_profile
+
+class round_profileTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
