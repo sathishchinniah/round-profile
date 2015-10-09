90b9543aeb6ecd5ae96376dab954ae544dc8ecdc
sathish chinniah <sathish73ec@gmail.com>
2015-10-09 14:11:06 +0530

    first

--- round profile/ViewController.swift
+++ round profile/ViewController.swift
@@ -9,15 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var image: UIImageView!
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//        var image: UIImage = UIImage(named: "1.jpg")
+        image.layer.borderWidth = 1
+        image.layer.masksToBounds = false
+        image.layer.borderColor = UIColor.blackColor().CGColor
+        image.layer.cornerRadius = image.frame.height/2
+        image.clipsToBounds = true
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        
+        
     }
 
 
